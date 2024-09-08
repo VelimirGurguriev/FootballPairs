4f35b27b8692549703e28ae4a74e8308518139c5
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-09-08 17:37:12 +0300

    Created project.

---
+++ src/main/java/com/exam/FootballPairs/FootballPairsApplication.java
@@ -0,0 +1,13 @@
+package com.exam.FootballPairs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballPairsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballPairsApplication.class, args);
+	}
+
+}
