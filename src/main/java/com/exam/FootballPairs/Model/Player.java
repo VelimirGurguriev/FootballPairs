b9c46b2cc6112b13997fe43b1d0969def044783a
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-09-09 14:29:59 +0300

    Created Player class.

---
+++ src/main/java/com/exam/FootballPairs/Model/Player.java
@@ -0,0 +1,60 @@
+package com.exam.FootballPairs.Model;
+
+public class Player {
+    private long id;
+    private int teamNumber;
+    private String position;
+    private String fullName;
+    private int teamId;
+
+    public Player() {
+    }
+
+    public Player(long id, int teamNumber, String position, String fullName, int teamId) {
+        this.id = id;
+        this.teamNumber = teamNumber;
+        this.position = position;
+        this.fullName = fullName;
+        this.teamId = teamId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getTeamNumber() {
+        return teamNumber;
+    }
+
+    public void setTeamNumber(int teamNumber) {
+        this.teamNumber = teamNumber;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(int teamId) {
+        this.teamId = teamId;
+    }
+}
