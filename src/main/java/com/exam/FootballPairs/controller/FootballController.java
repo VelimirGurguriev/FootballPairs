81638aace0746c0587cbd42392feee10b4fff32a
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-09-11 01:03:04 +0300

    Created Football controller class.

---
+++ src/main/java/com/exam/FootballPairs/controller/FootballController.java
@@ -0,0 +1,22 @@
+package com.exam.FootballPairs.controller;
+
+import com.exam.FootballPairs.service.FootballService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/football")
+public class FootballController {
+
+    @Autowired
+    private FootballService footballService;
+
+    @GetMapping("/teams")
+    public String viewTeams(Model model) {
+        model.addAttribute("teams", footballService.getTeams());
+        return "teams";
+    }
+}
