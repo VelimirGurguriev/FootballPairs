56b73b6fcdb81d066b47920af70e40708e96f7c6
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-09-10 16:05:26 +0300

    Created Football service class to call the CSV methods.

--- src/main/java/com/exam/FootballPairs/service/FootballService.java
+++ src/main/java/com/exam/FootballPairs/service/FootballService.java
@@ -1,4 +1,31 @@
 package com.exam.FootballPairs.service;
 
+import com.exam.FootballPairs.model.Match;
+import com.exam.FootballPairs.model.Player;
+import com.exam.FootballPairs.model.Record;
+import com.exam.FootballPairs.model.Team;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class FootballService {
+
+    @Autowired
+    public CsvService csvService;
+
+    public List<Player> getPlayers() {
+        return csvService.readPlayers();
+    }
+
+    public List<Team> getTeams() {
+        return csvService.readTeams();
+    }
+
+    public List<Match> getMatches() {
+        return csvService.readMatches();
+    }
+
+    public List<Record> getRecords() {
+        return csvService.readRecords();
+    }
 }
