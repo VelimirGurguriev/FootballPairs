f4a47c3b5acd2c17971b2947033691a71ddd1bd8
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-09-10 14:41:48 +0300

    Created Record class.

---
+++ src/main/java/com/exam/FootballPairs/Model/Record.java
@@ -0,0 +1,60 @@
+package com.exam.FootballPairs.Model;
+
+public class Record {
+    private long id;
+    private int playerId;
+    private int matchId;
+    private int fromMinutes;
+    private int toMinutes;
+
+    public Record() {
+    }
+
+    public Record(long id, int playerId, int matchId, int fromMinutes, int toMinutes) {
+        this.id = id;
+        this.playerId = playerId;
+        this.matchId = matchId;
+        this.fromMinutes = fromMinutes;
+        this.toMinutes = toMinutes;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    public int getMatchId() {
+        return matchId;
+    }
+
+    public void setMatchId(int matchId) {
+        this.matchId = matchId;
+    }
+
+    public int getFromMinutes() {
+        return fromMinutes;
+    }
+
+    public void setFromMinutes(int fromMinutes) {
+        this.fromMinutes = fromMinutes;
+    }
+
+    public int getToMinutes() {
+        return toMinutes;
+    }
+
+    public void setToMinutes(int toMinutes) {
+        this.toMinutes = toMinutes;
+    }
+}
